5fce488846ad6f040e76922624c0d99ffb9914b6
Christopher <chris@spea.re>
2015-01-20 11:17:21 -0500

    Bump version, minor fix.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup (
   name = 'rhine',
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'Rhine Python Client',
   long_description = 'For documentation, please see [http://www.rhine.io/docs].',
   url = 'https://github.com/Speare/rhine-python',
   license = 'New BSD',
   author = 'Speare Inc.',
   author_email = 'admin@rhine.io',
-  py_modules = ['rhine'],
+  packages = ['rhine'],
+  package_dir = {'rhine': 'rhine'},
   include_package_data = True,
   install_requires = ['requests'],
   classifiers = [
